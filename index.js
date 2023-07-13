205b7243f03b48b2270ba3c1a67c7af7302cb121
Luciano Angeleri <lcnang@gmail.com>
2023-07-13 14:24:58 -0300

    Se agregaron las configuraciones iniciales y los middlewares

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import "dotenv/config"
+import "./src/database/dbConnection"
+
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port", ()=>{
+    console.log("En el puerto " + app.get("port"))
+}))
+
+app.use(cors());
+app.use(express.json())
+app.use(morgan("dev"))
+console.log(__dirname); 
+app.use(express.static(path.join(__dirname, "/public")));
